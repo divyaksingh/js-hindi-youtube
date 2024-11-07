638676b9acbcbaa8d0b1b09406a472abaac18c15
divyaksingh <138142540+divyaksingh@users.noreply.github.com>
2024-11-07 12:28:07 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version of JS
+
+//alert (3+3) /we are using nodejs and not browser
+
+let name = "Divya"
+let age = 28
+let isLoggedIn = false
+let state
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean  => true/false 
+// null => standalone value
+// undefined => 
+// symbol  => unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
